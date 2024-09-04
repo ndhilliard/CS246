2ce197ab56e3b60a32f66c8ed5003677d3ae997e
ndhilliard <n_hilliard@hotmail.com>
2024-09-03 20:52:09 -0400

    Update main.cpp
    Updated isSentence logic to fit lab1 definition of a sentence.

--- Labs/main.cpp
+++ Labs/main.cpp
@@ -6,55 +6,49 @@ using namespace std;
 const int arraySize = 3;
 
 // function prototypes
-bool IsSentence(const string& str);
+bool isSentence(const string& str);
 string Capitalize(const string& str);
 
 int main() {
 
     // creating testing array with "sentences"
-    string test[arraySize]{ "This is a valid sentence.","this is not a sentence","Is this a valid sentence?" };
+    string test[arraySize]{ "This is a valid sentence.","this sentence has a number 1.","Is this a valid sentence?" };
 
     // for loop that runs IfSentence test
     for (int i = 0; i < arraySize; i++) {
-        if (IsSentence(test[i]) == true) {
+        if (isSentence(test[i]) == true) {
             cout << "String " << i + 1 << " is true!" << endl;
-        } else {
-            cout << "String " << i +1 << " is false!" << endl;
+        }
+        else {
+            cout << "String " << i + 1 << " is false!" << endl;
         }
 
     }
-    
+
     cout << endl;
 
     // for loop that takes true values on IfSentences
     // and capatalizes the first letter in every word
     for (int i = 0; i < arraySize; i++) {
-        cout << Capitalize(test[i]) << endl;
+        if (isSentence(test[i]) == true) {
+            cout << Capitalize(test[i]) << endl;
+        }
     }
 }
 
 //function definitions
-
 // tests if the string is a sentence
-bool IsSentence(const string& str) {
-    if (str.empty()) {
+bool isSentence(const string& str) {
+    if (str.empty() || !isalpha(str[0])) {
         return false;
     }
-
-    char firstChar = str[0];
-    char lastChar = str[str.length() - 1];
-    
-    bool startsWithUppercase = (firstChar >= 'A' && firstChar <= 'z');
-    bool endsWithpuncuation = (lastChar == '.' || lastChar == '?' || lastChar == '!');
-
-    return startsWithUppercase && endsWithpuncuation;
 }
 
 // function that takes true values on IsSentences
 // and returns a capitalized version of the string
 string Capitalize(const string& str) {
-    if (!IsSentence(str)) {
-        return "";
+    if (!isSentence(str)) {
+        return " ";
     }
 
     string result = str;
@@ -66,12 +60,9 @@ string Capitalize(const string& str) {
             capitalizeNext = false;
         } else {
             result[i] = tolower(result[i]);
-            }
-        if (result[i] == ' ') {
+        } if (result[i] == ' ') {
             capitalizeNext = true;
         }
     }
     return result;
 }
-
-
