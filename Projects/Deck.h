55a053ac4c6a5ca1208d23eb87ff115321807d03
ndhilliard <n_hilliard@hotmail.com>
2024-10-24 17:52:04 -0400

    Add files via upload

---
+++ Projects/Deck.h
@@ -0,0 +1,21 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <stdexcept>
+#include <cmath>
+#include "Util.h"
+#include "Tools.h"
+
+namespace ds {
+	class Deck : public Collection {
+
+	};
+}
+#endif DECK_H
