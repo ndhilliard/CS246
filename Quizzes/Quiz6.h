3f10fc331560d45de86b098d98d7ea565cc5f65d
ndhilliard <n_hilliard@hotmail.com>
2024-11-07 15:12:55 -0500

    Add files via upload

---
+++ Quizzes/Quiz6.h
@@ -0,0 +1,20 @@
+template <class K, class V>
+void Put(const K& key, const V& value) {
+
+	if (root == nullptr) {
+		root = new Node<Pair<K,V>(Pair<K,V>(key, value);
+	} else {
+		Node temp = root;
+		while (temp != nullptr && temp->data.key() != key) {
+			temp = t->next;
+		}
+	}
+
+	if (temp == nullptr) {
+		root->prev = new Node<Pair<K, V>(Pair<K, V>(key, value);
+		root->prev->next = root;
+		root = root->prev;
+	} else {
+		t->data.value() = value;
+	}
+}
